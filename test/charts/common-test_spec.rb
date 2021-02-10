afc707d25fe6fccbd32968aa352788c66f0e45cf
Mikael Sennerholm <mikael@sennerholm.net>
2021-02-10 21:47:56 +0100

    Add support for tpl in ingress handling

--- test/charts/common-test_spec.rb
+++ test/charts/common-test_spec.rb
@@ -186,5 +186,133 @@ class Test < ChartTest
         jq('.spec.volumeClaimTemplates[0].spec.storageClassName', resource('StatefulSet')).must_equal values[:volumeClaimTemplates][0][:storageClass]
       end
     end
+
+    describe 'ingress' do
+      it 'should be disabled when ingress.enabled: false' do
+        values = {
+          ingress: {
+            enabled: false
+          }
+        }
+        chart.value values
+        assert_nil(resource('Ingress'))
+      end
+
+      it 'should be enabled when ingress.enabled: true' do
+        values = {
+          ingress: {
+            enabled: true
+          }
+        }
+
+        chart.value values
+        refute_nil(resource('Ingress'))
+      end
+
+      it 'ingress with hosts' do
+        values = {
+          ingress: {
+            hosts: [
+              {
+                host: 'hostname',
+                paths: [
+                  {
+                    path: '/'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+
+        chart.value values
+        jq('.spec.rules[0].host', resource('Ingress')).must_equal values[:ingress][:hosts][0][:host]
+        jq('.spec.rules[0].http.paths[0].path', resource('Ingress')).must_equal values[:ingress][:hosts][0][:paths][0][:path]
+      end
+
+      it 'ingress with hosts template is evaluated' do
+        expectedHostName = 'common-test.hostname'
+        values = {
+          ingress: {
+            hosts: [
+              {
+                hostTpl: '{{ .Release.Name }}.hostname',
+                paths: [
+                  {
+                    path: '/'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+
+        chart.value values
+        jq('.spec.rules[0].host', resource('Ingress')).must_equal expectedHostName
+        jq('.spec.rules[0].http.paths[0].path', resource('Ingress')).must_equal values[:ingress][:hosts][0][:paths][0][:path]
+      end
+
+      it 'ingress with hosts and tls' do
+        values = {
+          ingress: {
+            enabled: true,
+            hosts: [
+              {
+                host: 'hostname',
+                paths: [
+                  {
+                    path: '/'
+                  }
+                ]
+              }
+            ],
+            tls: [
+              {
+                hosts: [ 'hostname' ],
+                secretName: 'hostname-secret-name'
+              }
+            ]
+          }
+        }
+
+        chart.value values
+        jq('.spec.rules[0].host', resource('Ingress')).must_equal values[:ingress][:hosts][0][:host]
+        jq('.spec.rules[0].http.paths[0].path', resource('Ingress')).must_equal values[:ingress][:hosts][0][:paths][0][:path]
+        jq('.spec.tls[0].hosts[0]', resource('Ingress')).must_equal values[:ingress][:tls][0][:hosts][0]
+        jq('.spec.tls[0].secretName', resource('Ingress')).must_equal values[:ingress][:tls][0][:secretName]
+      end
+
+      it 'ingress with hosts and tls templates is evaluated' do
+        expectedHostName = 'common-test.hostname'
+        expectedSecretName = 'common-test-hostname-secret-name'
+        values = {
+          ingress: {
+            enabled: true,
+            hosts: [
+              {
+                hostTpl: '{{ .Release.Name }}.hostname',
+                paths: [
+                  {
+                    path: '/'
+                  }
+                ]
+              }
+            ],
+            tls: [
+              {
+                hostsTpl: [ '{{ .Release.Name }}.hostname' ],
+                secretNameTpl: '{{ .Release.Name }}-hostname-secret-name'
+              }
+            ]
+          }
+        }
+
+        chart.value values
+        jq('.spec.rules[0].host', resource('Ingress')).must_equal expectedHostName
+        jq('.spec.rules[0].http.paths[0].path', resource('Ingress')).must_equal values[:ingress][:hosts][0][:paths][0][:path]
+        jq('.spec.tls[0].hosts[0]', resource('Ingress')).must_equal expectedHostName
+        jq('.spec.tls[0].secretName', resource('Ingress')).must_equal expectedSecretName
+      end
+    end
   end
 end
