ff9781b543b5a8fd1950e7114e01e86e4401e068
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-01-05 19:42:42 +0100

    Simplify renovate script
    Signed-off-by: Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>

--- hack/renovate-releasenotes.sh
+++ hack/renovate-releasenotes.sh
@@ -28,20 +28,10 @@ if [ $# -ge 1 ] && [ -n "$1" ]; then
     NEW_DEPENDENCIES=$(cat Chart.yaml | yq e '.dependencies[].name' -P - | LC_ALL=C sort)
     OLD_DEPENDENCIES=$(git show origin/$DEFAULT_BRANCH:./Chart.yaml | yq e '.dependencies[].name' -P - | LC_ALL=C sort)
 
-    ADDED_DEPENDENCIES=$(comm -23 <(printf '%s\n' "${NEW_DEPENDENCIES[@]}") <(printf '%s\n' "${OLD_DEPENDENCIES[@]}"))
-    UPDATED_DEPENDENCIES=$(comm -12 <(printf '%s\n' "${NEW_DEPENDENCIES[@]}") <(printf '%s\n' "${OLD_DEPENDENCIES[@]}"))
-    REMOVED_DEPENDENCIES=$(comm -13 <(printf '%s\n' "${NEW_DEPENDENCIES[@]}") <(printf '%s\n' "${OLD_DEPENDENCIES[@]}"))
-
     tmpfile=$(mktemp)
     trap 'rm -f "$tmpfile"' EXIT
 
-    for DEP_NAME in ${ADDED_DEPENDENCIES[@]}
-    do
-      echo "- kind: changed" >> $tmpfile
-      echo "  description: Added \`${DEP_NAME}\` chart dependency." >> $tmpfile
-    done
-
-    for DEP_NAME in ${UPDATED_DEPENDENCIES[@]}
+    for DEP_NAME in ${NEW_DEPENDENCIES[@]}
     do
       NEW_VERSION=$(cat Chart.yaml | yq e ".dependencies[] | select(.name == \"$DEP_NAME\") | .version" -P -)
       OLD_VERSION=$(git show origin/$DEFAULT_BRANCH:./Chart.yaml | yq e ".dependencies[] | select(.name == \"$DEP_NAME\") | .version" -P -)
@@ -49,12 +39,6 @@ if [ $# -ge 1 ] && [ -n "$1" ]; then
         echo "- kind: changed" >> $tmpfile
         echo "  description: Upgraded \`${DEP_NAME}\` chart dependency to version \`${NEW_VERSION}\`." >> $tmpfile
       fi
-    done
-
-    for DEP_NAME in ${REMOVED_DEPENDENCIES[@]}
-    do
-      echo "- kind: changed" >> $tmpfile
-      echo "  description: Removed \`${DEP_NAME}\` chart dependency." >> $tmpfile
     done
 
     yq eval-all --inplace 'select(fileIndex == 0).annotations."artifacthub.io/changes" = (select(fileIndex == 1) | to_yaml) | select(fileIndex==0)' Chart.yaml $tmpfile
